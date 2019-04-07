c5c0bb4111fcc40de6e56907a5d7ed8c47530915
Gerd Langer <gerd.langer@gmx.de>
2019-04-07 18:19:50 +0200

    joy-pins erweitert

--- shmup/joystickpins.py
+++ shmup/joystickpins.py
@@ -2,7 +2,7 @@ from pygame import joystick
 
 #Pins-Mapping: (A, B, X, Y, SELECT, START, SHOULDER_LEFT, SHOULDER_RIGHT, AXIS_X, AXIS_Y)
 joystick_mappings = {
-            "USB Gamepad " :       {
+            "USB Gamepad" :       {
                 "A"         : 1,
                 "B"         : 2,
                 "X"         : 0,
@@ -25,27 +25,41 @@ joystick_mappings = {
                 "SH_RIGHT"  : 6,
                 "AXIS_X"    : 0,  # -1 = links, +1 = rechts
                 "AXIS_Y"    : 1   # -1 = oben,  +1 = unten
-            }
+            },
+            "PLAYSTATION(R)3 Controller" : {
+                "A"         : 13,   # circle
+                "B"         : 14,   # cross
+                "X"         : 12,   # triangle
+                "Y"         : 15,   # square
+                "SELECT"    : 0,
+                "START"     : 3,
+                "SH_LEFT"   : 10,   # L1
+                "SH_RIGHT"  : 11,   # R1
+                "AXIS_X"    : 0,  # -1 = links, +1 = rechts
+                "AXIS_Y"    : 1   # -1 = oben,  +1 = unten
+            },
         }
 
 class JoystickPins():
     def __init__(self, joystick, mapping = None):
         self.joystick = joystick
-        self.name = joystick.get_name()
+        self.name = joystick.get_name().strip()
         if mapping is not None:
             self.mapping = mapping
-        else:
+        elif self.name in joystick_mappings.keys():
             self.mapping = joystick_mappings[self.name]
-        self._A = self.mapping["A"]
-        self._B = self.mapping["B"]
-        self._X = self.mapping["X"]
-        self._Y = self.mapping["Y"]
-        self._select = self.mapping["SELECT"]
-        self._start  = self.mapping["START"]
-        self._shoulder_left  = self.mapping["SH_LEFT"]
-        self._shoulder_right = self.mapping["SH_RIGHT"]
-        self._axis_x = self.mapping["AXIS_X"]
-        self._axis_y = self.mapping["AXIS_Y"]
+        else:
+            self.mapping = {}
+        self._A = self.mapping.get("A")
+        self._B = self.mapping.get("B")
+        self._X = self.mapping.get("X")
+        self._Y = self.mapping.get("Y")
+        self._select = self.mapping.get("SELECT")
+        self._start  = self.mapping.get("START")
+        self._shoulder_left  = self.mapping.get("SH_LEFT")
+        self._shoulder_right = self.mapping.get("SH_RIGHT")
+        self._axis_x = self.mapping.get("AXIS_X")
+        self._axis_y = self.mapping.get("AXIS_Y")
 
     def A(self):
         return self._A
@@ -67,3 +81,28 @@ class JoystickPins():
         return self._axis_x
     def axis_y(self):
         return self._axis_y
+
+    def get_A(self):
+        return self.joystick.get_button(self.A)
+    def get_B(self):
+        return self.joystick.get_button(self.B)
+    def get_X(self):
+        return self.joystick.get_button(self.X)
+    def get_Y(self):
+        return self.joystick.get_button(self.Y)
+    def get_select(self):
+        return self.joystick.get_button(self._select)
+    def get_start(self):
+        return self.joystick.get_button(self._start)
+    def get_shoulder_left(self):
+        return self.joystick.get_button(self._shoulder_left)
+    def get_shoulder_right(self):
+        return self.joystick.get_button(self._shoulder_right)
+    def get_axis_left(self):
+        return self.joystick.get_axis(self._axis_x) < -0.9
+    def get_axis_right(self):
+        return self.joystick.get_axis(self._axis_x) >  0.9
+    def get_axis_up(self):
+        return self.joystick.get_axis(self._axis_y) < -0.9
+    def get_axis_down(self):
+        return self.joystick.get_axis(self._axis_y) >  0.9
